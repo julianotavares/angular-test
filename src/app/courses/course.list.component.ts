d8d6535126728385b0b86a4f98903b70cf2308be
Juliano Tavares <juliano.tavaress@gmail.com>
2021-02-18 20:38:14 -0300

    added course.list.component

--- src/app/courses/course.list.component.ts
+++ src/app/courses/course.list.component.ts
@@ -1,9 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';"@angular/core";
+import { Course } from "./course";
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course.list.component.html'
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
 
+  courses: Course[] = [];
+
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular: Forms',
+        imageUrl: '',
+        price: 99.99,
+        code: 'XPS-8796',
+        duration: 120,
+        rating: 4.5,
+        releaseDate: 'November, 2, 2020'
+      },
+      {
+        id: 2,
+        name: 'Angular: HTTP',
+        imageUrl: '',
+        price: 45.99,
+        code: 'LKL-1094',
+        duration: 80,
+        rating: 4,
+        releaseDate: 'December, 4, 2020'
+      }
+    ];
+  }
 }

f133dd8794801f7aaaaa163f4b1a67eb957db3a7
Juliano Tavares <juliano.tavaress@gmail.com>
2021-02-19 14:10:29 -0300

    added pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course.list.component';
 import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
+    ReplacePipe,
   ],
   imports: [
     BrowserModule,
